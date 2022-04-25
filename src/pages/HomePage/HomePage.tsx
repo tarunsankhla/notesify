a79dddd1d1f6d04f28b57770419cd371f7e6d8a6
Tether <tarun.sankhla.prc@gmail.com>
2022-04-25 14:11:58 +0530

    added onchange

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,13 @@
 import "./HomePage.css";
 import ReactQuill from 'react-quill';
 import CreateButton from "../../components/UI/Buttons/CreateButton/CreateButton";
+import { useRef, useState } from "react";
+import { debounce } from "../../utils/Debounce";
 
 export default function HomePage() {
+  const [noteContent, setNoteContent] = useState("");
+  const noteRef = useRef("");
+
   var modules = {
     toolbar: [
       [{ 'header': [1, 2, false] }],
@@ -16,15 +21,28 @@ export default function HomePage() {
   var formats = [
     'header',
     'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet', 'indent',
+    'list', 'bullet', 'indent', 
     'link', 'image'
   ];
+
+  const OnUpdateNote = (text:any) => { 
+    console.log(text, noteContent);
+    var doc = new DOMParser().parseFromString(text, "text/xml");
+    console.log(doc.textContent,doc.firstChild?.textContent);
+    setNoteContent(text.innerText);
+  }
   return (
     <div>
       <div className="note-editor-container">
-        <ReactQuill theme="snow" modules={modules} formats={formats} />  
+        <ReactQuill
+          theme="snow"
+          modules={modules}
+          formats={formats}
+          placeholder={"Write Something....."}
+          value={noteContent}
+          onChange={(e) => debounce(() => OnUpdateNote(e), 500)} />  
         <div>
-          <div className="color-schema"></div>
+          <div className="color-schema">{ noteContent}</div>
           <CreateButton />
         </div>
       </div>
--- src/utils/Debounce.js
+++ src/utils/Debounce.js
@@ -4,3 +4,5 @@ export function debounce(callback, time) {
     window.clearTimeout(debouncertime);
     debouncertime = setTimeout(callback, time);
 }
+
+// document.querySelector('p').nodeValue
