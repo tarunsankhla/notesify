0f3eef85456a7cefdb24fb510b859232b90306b6
Tether <tarun.sankhla.prc@gmail.com>
2022-04-25 11:18:46 +0530

    fixed package and mockamn

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router';
 import { ROUTE_PATH_ArchivePage, ROUTE_PATH_LandingPage, ROUTE_PATH_Mockman, ROUTE_PATH_TrashPage } from './utils/Route';
-import HomePage  from "./pages/HomePage/HomePage";
+// import HomePage from "./pages/HomePage/HomePage";
+// import HomePage from './pages/HomePage/HomePage';
 // import { TrashPage } from "./pages/HomePage/TrashPage";
 // import { ArchivePage } from "./pages/HomePage/ArchivePage";
-import Mockman from "mockman-js";
 import Main from './Main';
 import React, { Suspense } from 'react';
+// const {Mockman} = require("mockman-js");
+import Mockman from 'mockman-js';
 
-// const HomePage = React.lazy(() => import('./pages/HomePage/HomePage'));
+const HomePage = React.lazy(() => import('./pages/HomePage/HomePage'));
 const TrashPage = React.lazy(() => import('./pages/TrashPage/TrashPage'));
 const ArchivePage = React.lazy(() => import('./pages/ArchivePage/ArchivePage'));
 
@@ -17,16 +19,14 @@ function App() {
 
   return (
     <div className="App">
-      {/* <header className="App-header">
-      
-      </header> */}
+  
       <Routes >
         <Route element={<Main />}>
-          <Route path='/' element={<HomePage />}></Route>
-          {/* <Route path={ROUTE_PATH_LandingPage} element={
+          {/* <Route path='/' element={<HomePage />}></Route> */}
+          <Route path={ROUTE_PATH_LandingPage} element={
                 <Suspense fallback={<h1>Loading Home...</h1>}>
                   <HomePage />
-                </Suspense>} /> */}
+                </Suspense>} />
           <Route path={ROUTE_PATH_TrashPage} element={
                 <Suspense fallback={<h1>Loading Trash...</h1>}>
                   <TrashPage />
@@ -41,5 +41,6 @@ function App() {
     </div>
   );
 }
-  {/* <input onChange={(e) => debounce(()=>update(e.target.value), 500)} /> */}
+{/* <input onChange={(e) => debounce(()=>update(e.target.value), 500)} /> */ }
+  // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiJmNTg4Y2NmOC1jYjhkLTRjMzAtYjg2MC1lNmM3ZjQwZmNmYjIiLCJlbWFpbCI6ImFkYXJzaGJhbGlrYUBnbWFpbC5jb20ifQ.AMka3wzV3nXZA2OEKejEdL546VHpTIrP1GszqcbCbO8
 export default App;
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,69 @@
+// import React from 'react';
+import React, { SVGProps } from 'react'
+import {BiTrash} from '../../components/UI/Icons/BiTrash';
+import "./HomePage.css";
+// import {BiTrash} from "../../components/UI/Icons/BiTrash";
+
+export default function HomePage(){
+  return (
+    <div>HomePage
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      {/* <BiTrash /> */}
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      <div>HomePage</div>
+      
+    </div>
+  )
+}
+
+
+
+// export default HomePage   
